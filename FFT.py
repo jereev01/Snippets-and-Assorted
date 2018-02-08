d21e977cd52a56372ecb4e7719c48bfe2571b8c7
jereev01 <34323495+jereev01@users.noreply.github.com>
2018-02-08 13:58:19 -0500

    Add files via upload
    Updated FFT to include phase correction, zero filling and correct f-axis scaling

--- FFT.py
+++ FFT.py
@@ -1,32 +1,111 @@
 # -*- coding: utf-8 -*-
 """
-FFT Basics
+Created on Mon Dec 11 12:02:50 2017
+
+@author: jbr2
 """
+
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-f = 18
-num = 100
-start = -1
-end = 1
-step = (end - start)/num
+test_data = pd.read_csv('BG_Au_R_int.0.dpt', header = None)
+test_data = test_data.rename(index = str, columns = {0 : 'measurement', 1 : 'amplitude'})
+test_data = test_data.drop('measurement', axis = 1)
 
-test_x = np.array([(step*x + start) for x in range(num+1)])                 
-test_y = np.array([np.e**(-x**2/(2*0.1**2))*(np.cos(2*np.pi*f*x)+0.2*np.cos(2*np.pi*0.7*f*x)) for x in test_x])
+data = np.array(test_data['amplitude'])
 
-#plt.plot(test_x,test_y,"o-")
-fourier = np.fft.fft(test_y) #question for self: what are different ffts in py
+ref = pd.read_csv('BG_Au_R_spect.0.dpt', header = None)
+ref = ref.rename(index = str, columns = {0 : 'measurement', 1 : 'amplitude'})
+refdata = np.array(ref['amplitude'])
+refwave = np.array(ref['measurement'])
 
-pwr_spct = list(np.abs(fourier)**2)
+
+laser_wavenumber = 15797.09
+data_length = int(len(data)/2)
+
+forward = data[:data_length]
+backward = data[data_length:]
+
 
 
 """
-assign absolute frequencies to the data. 
-presently: must have an even number of data points, but can be generalized
+Normalize
 """
 
-fourier_x = [1/(step*num)*x for x in range(num//2+1)]
-fx = fourier_x + [-1*x for x in fourier_x[:0:-1]]
+normal_forward = forward - np.mean(forward)
 
-plt.plot(fx,pwr_spct,"o-")
+"""
+Zero-Fill
+"""
 
+log_size = int(np.log2(data_length))
+zerofill_factor = 2
+num_zeros = 2**(zerofill_factor + log_size) - data_length
+zero_pad = [0] * num_zeros
+normal_pad_forward = np.append(normal_forward, zero_pad)
+
+"""
+Calculate Mertz Phase Correction Data
+"""
+
+phase_data_size = 128
+
+zpd = np.argmax(normal_pad_forward)
+zpd_data = normal_pad_forward[zpd-phase_data_size:zpd+phase_data_size]
+mertz = [0] * len(normal_pad_forward)
+
+for i, j in zip(range(zpd-phase_data_size,zpd+phase_data_size), range(0,phase_data_size*2)):
+    mertz[i] = (1-np.abs((zpd - i)/phase_data_size))*zpd_data[j] 
+    
+mertz = mertz[zpd:] + mertz[:zpd]
+
+fftmertz = np.fft.fft(mertz)
+fftmertzR = np.real(fftmertz)
+fftmertzI = np.imag(fftmertz)
+mertzpwrspct = np.abs(fftmertz)
+
+
+phase_dataR = mertzpwrspct * fftmertzR
+phase_dataI = mertzpwrspct * fftmertzI
+
+
+"""
+Mertz Correction Apodization
+"""
+
+apocorrection = []
+for i in range(2*zpd):
+    apocorrection.append(zpd/0.5 * i)
+for i in range(2*zpd,len(normal_forward)):
+    apocorrection.append(1 - len(normal_forward) * (i - 2*zpd))
+for i in range(len(normal_forward),len(normal_pad_forward)):
+    apocorrection.append(0)
+
+
+apoforward = list(normal_pad_forward * apocorrection)
+apoforward = apoforward[zpd:] + apoforward[:zpd]
+
+"""
+fft
+"""
+
+spectrum_size = len(apoforward)//2
+max_wavenumber = laser_wavenumber
+spacing = max_wavenumber/spectrum_size
+freq_data = [spacing * i for i in range(spectrum_size)]
+                
+
+fftspectrum = 1/(4*spectrum_size*laser_wavenumber)*np.fft.fft(apoforward)[:spectrum_size]
+fftspectrumR = np.real(fftspectrum)
+fftspectrumI = np.imag(fftspectrum)
+
+
+plt.plot(refwave,refdata,"o")
+plt.plot(freq_data,np.abs(fftspectrum),"o")
+plt.xlim(2000,2010)
+
+print(max(refdata))
+print(max(np.abs(fftspectrum)))
+
+print(max(refdata)/max(np.abs(fftspectrum)))
